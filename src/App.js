894c6a9c2bda2caa1a8e43e52e38b13de8766dae
Tigran <gvideo2007@gmail.com>
2022-06-30 15:18:47 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import useInput from './hooks/useInput';
 
 function App() {
+  const userName = useInput('');
+  const password = useInput('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input {...userName} placeholder='user name'/>
+      <input {...password}  placeholder='user password'/>
+      <button onClick={() => console.log(userName.value, password.value)}>console log value</button>
     </div>
   );
 }
