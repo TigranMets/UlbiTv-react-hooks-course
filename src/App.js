167b13e863eb1fd5e2b25679680634b12ed77eae
Tigran <gvideo2007@gmail.com>
2022-07-02 16:06:58 +0400

    Adding useScroll hook.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { useRef } from 'react';
 import './App.css';
-import Hover from './components/Hover';
-import useHover from './hooks/useHover';
+import List from './components/List';
+// import Hover from './components/Hover';
+// import useHover from './hooks/useHover';
 
 function App() {
-  let ref = useRef();
-  const isBlackHovering = useHover(ref)
   return (
     <div className='App'>
-      <Hover />
-      <div ref={ref} style={{ width: 300, height: 300, backgroundColor: isBlackHovering ? 'blue' : 'black' }}></div>
+      <List />
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react'
+import useScroll from '../hooks/useScroll';
+
+const List = () => {
+    let [todos, setTodos] = useState([]);
+    let [page, setPage] = useState(1);
+    let [limit, setLimit] = useState(20);
+    const parentRef = useRef();
+    const childRef = useRef();
+    const intersected = useScroll(parentRef, childRef, () => fetchTodos(page, limit));
+
+    function fetchTodos(page, limit) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
+            .then(response => response.json())
+            .then(json => {
+                setTodos(prev => [...prev, ...json]);
+                setPage(prev => prev + 1);
+            }
+            )
+    };
+
+    return (
+        <div ref={parentRef} style={{height: '88vh', overflow: 'auto'}}>
+            {todos.map(todo => <div key={todo.id} style={{ padding: 30, border: '2px solid black' }}>
+                {todo.id}. {todo.title}
+            </div>)}
+            <div ref={childRef} style={{ height: 30, backgroundColor: 'red' }}></div>
+        </div>
+    )
+}
+
+export default List
