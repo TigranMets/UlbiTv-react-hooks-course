47697e7c0fed3c6aaf07bc5d2139aab3324730f6
Tigran <gvideo2007@gmail.com>
2022-07-03 17:11:47 +0400

    Adding useDebounce hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
-import List from './components/List';
+// import List from './components/List';
+import useDebounce from './hooks/useDebounce';
 // import Hover from './components/Hover';
 // import useHover from './hooks/useHover';
 
 function App() {
+
+  const [value, setValue] = useState('');
+  const debouncedSearch = useDebounce(search, 2000);
+  
+  const onChange = (e) => {
+    setValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
+
+  function search(query) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
+      .then(response => response.json())
+      .then(json => console.log(json)
+      );
+  };
+  
   return (
     <div className='App'>
-      <List />
+      <input value={value} onChange={onChange} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import useScroll from '../hooks/useScroll';
 
 const List = () => {
